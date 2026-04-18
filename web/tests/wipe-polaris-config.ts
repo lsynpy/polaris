c397845531ca2be3c470b406925cf50c5dd600ee
lifshort <cw.pypy@gmail.com>
2026-04-18 10:42:01 +0800

    refactor: remove unused wipe-polaris-config.ts
    This file was deleted as it was not referenced in the Playwright configuration.

--- web/tests/wipe-polaris-config.ts
+++
@@ -1,9 +0,0 @@
-import fs from "node:fs";
-import type { FullConfig } from "@playwright/test";
-
-async function globalSetup(_config: FullConfig) {
-  await fs.writeFile("automated.config.toml", "", () => {});
-  await new Promise((resolve) => setTimeout(resolve, 2000)); // Give server time to detect and apply config change
-}
-
-export default globalSetup;
